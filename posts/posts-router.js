2943200cdb0e9fd646b4dc03d021593b51159a53
Alex Kemper <alexkemper@MacBook-Pro.local>
2020-08-04 21:53:20 -0400

    post new comment is working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -36,4 +36,61 @@ router.post("/", (req, res) => {
   }
 });
 
+router.post("/:id/comments", (req, res) => {
+  let postID = req.params.id;
+  let newComment = {
+    text: req.body.text,
+    post_id: postID,
+  };
+
+  if (!newComment.text) {
+    res
+      .status(400)
+      .send({ errorMessage: "Please provide text for the comment." });
+  } else {
+    Posts.findById(postID)
+      .then((response) => {
+        let postRequested = response;
+        console.log(postRequested);
+
+        Posts.insertComment(newComment)
+          .then((response) => {
+            console.log(response);
+
+            Posts.findCommentById(response.id)
+              .then((response) => {
+                console.log(response);
+
+                let commentReturned = response;
+
+                return res.status(201).send(commentReturned);
+              })
+              .catch((error) => {
+                console.log(error);
+
+                return res.status(500).send({
+                  error:
+                    "There was an error while saving the comment to the database",
+                });
+              });
+          })
+          .catch((error) => {
+            console.log(error);
+
+            return res.status(500).send({
+              error:
+                "There was an error while saving the comment to the database",
+            });
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+
+        res
+          .status(404)
+          .send({ message: "The post with the specified ID does not exist." });
+      });
+  }
+});
+
 module.exports = router;
